6de3a5d8b1313c03e7e29268871f406462c8bf54
dragoon20 <fernandojordan.92@gmail.com>
2013-09-24 11:42:20 +0700

    Added template play framework project, already connected to mysql, use the sql file from data repository.

---
+++ AASA/target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -0,0 +1,55 @@
+
+package views.html
+
+import play.templates._
+import play.templates.TemplateMagic._
+
+import play.api.templates._
+import play.api.templates.PlayMagic._
+import models._
+import controllers._
+import java.lang._
+import java.util._
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import play.api.i18n._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.data._
+import play.api.data.Field
+import play.mvc.Http.Context.Implicit._
+import views.html._
+/**/
+object index extends BaseScalaTemplate[play.api.templates.HtmlFormat.Appendable,Format[play.api.templates.HtmlFormat.Appendable]](play.api.templates.HtmlFormat) with play.api.templates.Template1[String,play.api.templates.HtmlFormat.Appendable] {
+
+    /**/
+    def apply/*1.2*/(message: String):play.api.templates.HtmlFormat.Appendable = {
+        _display_ {
+
+Seq[Any](format.raw/*1.19*/("""
+
+"""),_display_(Seq[Any](/*3.2*/main("Axe Appolo Space Academy")/*3.34*/ {_display_(Seq[Any](format.raw/*3.36*/("""
+
+    """),_display_(Seq[Any](/*5.6*/play20/*5.12*/.welcome(message, style = "Java"))),format.raw/*5.45*/("""
+
+""")))})),format.raw/*7.2*/("""
+"""))}
+    }
+    
+    def render(message:String): play.api.templates.HtmlFormat.Appendable = apply(message)
+    
+    def f:((String) => play.api.templates.HtmlFormat.Appendable) = (message) => apply(message)
+    
+    def ref: this.type = this
+
+}
+                /*
+                    -- GENERATED --
+                    DATE: Tue Sep 24 00:15:00 WIT 2013
+                    SOURCE: /home/jordan/AASA/app/views/index.scala.html
+                    HASH: 277a164335f7e6cf857bc520bb50e9385ccc548e
+                    MATRIX: 774->1|885->18|922->21|962->53|1001->55|1042->62|1056->68|1110->101|1143->104
+                    LINES: 26->1|29->1|31->3|31->3|31->3|33->5|33->5|33->5|35->7
+                    -- GENERATED --
+                */
+            
